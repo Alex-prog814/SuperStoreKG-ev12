6f238f5e1c0418187ca416cefc70a65c8b699885
Alex <alex@MacBook-Air-Alex.local>
2022-11-24 19:28:01 +0600

    First commit

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -9,4 +9,6 @@ export const CART = {
 };
 
 // export const JSON_API_PRODUCTS = 'http://localhost:8000/products';
-export const JSON_API_PRODUCTS = 'https://super-store-kg.vercel.com/api/products';
+export const JSON_API_PRODUCTS = 'https://super-store-kg-ev12.vercel.app/api/products';
+
+// https://super-store-kg-ev12.vercel.app/products?q=&type=clothes
